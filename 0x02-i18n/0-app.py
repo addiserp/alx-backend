d228c629c4aa3d054c0f21af7a30890603b23e5b
Mikias Gedlu <addiserp@gmail.com>
2024-04-09 16:44:13 +0300

    0x02. i18n 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,13 +7,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def index():
-    """0x02. i18n"""
+    """
+        index start point for 0x02. i18n
+    """
 
     return render_template('0-index.html')
 
 
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host='0.0.0.0', port=5000,debug=True)
+    app.run(debug=True)
