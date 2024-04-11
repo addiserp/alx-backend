ffbb340bf539d955496933a74b1f1325e696caaf
Mikias Gedlu <addiserp@gmail.com>
2024-04-11 17:16:23 +0300

    0x02-i18n hotfix 4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -39,6 +39,5 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-babel.init_app(app, locale_selector=get_locale)
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
