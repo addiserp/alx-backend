30a4a4afab8584a7ebee8975e7ccacbe155775c4
Mikias Gedlu <addiserp@gmail.com>
2024-04-11 17:14:15 +0300

    0x02-i18n hotfix 4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python3
-'''
-    Force locale with URL parameter
-'''
-
-from flask import Flask, render_template, request
+"""
+    Starts a Flash-babel Web Application
+"""
 from flask_babel import Babel
+from flask import Flask, render_template, request
 
 
-class Config:
-    '''
-    It's a config class
-    '''
-
-    DEBUG = True
+class Config(object):
+    """
+    Babel Configuration setup
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -20,39 +17,27 @@ class Config:
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
-@babel.localeselector
-def get_locale() -> str:
+@app.route('/', strict_slashes=False)
+def index() -> str:
     """
-    It retrieves the locale for a web page.
+    home page index
+    """
+    return render_template('4-index.html')
 
-    Returns:
-        str: best match
+
+# @babel.localeselector
+def get_locale():
+    """
+    the Babel extension in Flask for locale
     """
     locale = request.args.get('locale')
     if locale in app.config['LANGUAGES']:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-
-@app.route('/')
-def index() -> str:
-    '''
-    It's a default route
-
-    Returns:
-        html: homepage
-    '''
-    return render_template("4-index.html")
-
-# uncomment this line and comment the @babel.localeselector
-# you get this error:
-# AttributeError: 'Babel' object has no attribute 'localeselector'
-# babel.init_app(app, locale_selector=get_locale)
-
-
+babel.init_app(app, locale_selector=get_locale)
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000, debug=True)
